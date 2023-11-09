94b4bdf2cd825fb45ed9a04954c29d711563abbb
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2023-11-08 19:07:40 -0500

    feat: JS-HTML-CSS Almacenamiento Local
    - Almacenar Localmente.
    - Recuperar lo salvado.
    - Correcciones a la adicción de tareas y el uso del "checked" en el checkbox.
    
    #20231107c

--- 20231107-Javascript_vanilla/main.js
+++ 20231107-Javascript_vanilla/main.js
@@ -13,10 +13,30 @@ const app = {
   newTaskInput: newTaskInput,
 }
 
+// Grabamos el arreglo en el `LocalStorage`
+function saveTasksToLocaStorage(tasks) {
+  // Salvamos usando la API del browser
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+//En el momento de cargar la pantalla, leemos el `LocalStorage`
+window.onload = () => {
+  // Tomamos el `LocalStorage` y lo convertimos en un objeto
+  const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  // cargamos el arrelo, con los valores recorridos por un `map`
+  app.tasks = savedTasks.map((task) => {
+    return createTask(task.title, task.isCompleted, task.id);
+  });
+  // Recorremos el arrelo con un `forEach` y se muestran en pantalla o HTML
+  app.tasks.forEach((task) => {
+    return addTaskToList(task, app.tasksList);
+  })
+}
+
 // Función que crea el objeto de la tarea
-function createTask(title, isCompleted = false) {
+function createTask(title, isCompleted = false, id = Date.now()) {
   return {
-    id: Date.now(),
+    id,
     title,
     isCompleted
   }
@@ -37,9 +57,11 @@ function addTask(app) {
   // Llamamos la función de crear la tarea, q nos retorna un objeto
   const newTask = createTask(newTaskTitle);
   // Añadimos al arreglo
-  tasks.push(newTask);
+  app.tasks.push(newTask);
   // Lllamamo la funcion para renderizar en pantall
   addTaskToList(newTask, app.tasksList);
+  // Justo cuando se muestra en pantalla se almacena en el `LocalStorage`
+  saveTasksToLocaStorage(app.tasks);
   // Limpiamos el valor del input
   app.newTaskInput.value = '';
 }
@@ -53,19 +75,23 @@ function createTaskElement(task) {
   // Añadimos el tipo para saber como se va a representar
   taskCheckbox.type = 'checkbox';
   // El valor sería lo q esté en `isCompleted`
-  taskCheckbox.value = task.isCompleted;
+  taskCheckbox.checked = task.isCompleted;
   // Añadimos una Evento-Escucha
-  taskCheckbox.addEventListener('click', () => { // 'change', () => {
+  taskCheckbox.addEventListener('change', () => { // 'click', () => {
     // Ponemos en la tarea el valor o el estado de `checked`
     task.isCompleted = taskCheckbox.checked;
     // Añadimos o quitamos una clase al elemento
     taskText.classList.toggle('completed', task.isCompleted);
+    // Justo despues de alterar el arreglo, lo volvemos a grabar en el `localStoarage`
+    saveTasksToLocaStorage(app.tasks);
   });
 
   // Añadimos un elemento de `taskText` de tipo `span`
   const taskText = document.createElement('span');
   // Ponemos el contenido con el titulo de la tarea
   taskText.textContent = task.title;
+  // Reconfirmo la clase "completed"
+  taskText.classList.toggle("completed", task.isCompleted);
   // Creamos un elemento de tipo `button`, para borrar la tarea
   const taskDeleteButton = document.createElement('button');
   // Le ponemos el nombre a mostar o emoji
@@ -74,7 +100,18 @@ function createTaskElement(task) {
   taskDeleteButton.className = 'delete-button';
   // Añadimos el evento-escuha
   taskDeleteButton.addEventListener('click', () => {
-    console.log('Eliminamos la tarea de la lista:', task.id);
+    // console.log('Eliminamos la tarea de la lista:', task.id);
+    // Removemos el elemento del DOM o HTML
+    taskElement.remove();
+    // Guardamos el índice del arreglo
+    const taskIndex = app.tasks.indexOf(task);
+    // Si el índice es diferente de -1, es porque existe
+    if (taskIndex !== -1) {
+      // Lo borramos simplemente
+      app.tasks.splice(taskIndex, 1);
+    }
+    // Justo despues de alterar el arreglo, lo volvemos a grabar en el `localStoarage`
+    saveTasksToLocaStorage(app.tasks);
   })
 
   // Añadimos el elemento del checkbox en el HTML
@@ -89,15 +126,17 @@ function createTaskElement(task) {
 }
 
 // Evento-Escucha para el botón de `Ingresar Tarea`
-addTaskButton.addEventListener("click", ()=>{
+addTaskButton.addEventListener("click", () => {
   // Lamamos simplemente la función addTask()
   addTask(app);
 })
 
 // Evento-escucha para cuando presiono cualquier tecla, lueggo valido cual
-newTaskInput.addEventListener("keydown", (event)=>{
+newTaskInput.addEventListener("keydown", (event) => {
   // Pregunto si la tecla es el `Enter`
-  if (event.key === "Enter"){
+  if (event.key === "Enter") {
     addTask(app);
   }
 })
+
+//
