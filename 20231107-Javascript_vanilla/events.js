b07d65fe34d63da077dca7ae8347cf2f7cfb7252
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2023-11-13 19:45:45 -0500

    feat: JS-HTML-CSS Editando Tarea
    - Boton nuevo para Cancelar en el index.html.
    - Funcionando el Boton de Editar, grabar lo Editado y Cancelar.
    
    #20231107e

--- 20231107-Javascript_vanilla/events.js
+++ 20231107-Javascript_vanilla/events.js
@@ -24,7 +24,19 @@ const onClickDeleteButton = (task, taskElement) => {
 }
 
 const onClickEditButton = (task, taskElement) => {
-  console.log('Editamos la tarea', task.id, taskElement);
+  // console.log('Editamos la tarea', task.id, taskElement, task);
+  // Ponemos el texto en el input para ser modificado 
+  app.newTaskInput.value = task.title;
+  // Cambia el botón de Ingresar Tarea por "Editar Tarea"
+  app.addTaskButton.textContent = app.EDITTASK.text;
+  // Solo hacemos el cambio si la clase es "add-task-btn"
+  if (app.addTaskButton.classList.contains("add-task-btn")) {
+    // Cambiamos el color del botón principal por editar
+    app.addTaskButton.classList.toggle(app.ADDTASK.class);
+    app.addTaskButton.classList.toggle(app.EDITTASK.class);
+  }
+  // Guardo el Id para saber q voy a modificar en el arreglo y en pantalla
+  app.editingId = task.id;
 }
 
 const onClickAddButton = () => {
@@ -32,9 +44,19 @@ const onClickAddButton = () => {
   addTask(app);
 }
 
-const onKeyDownInput =(evt) => {
+const onClickCancelButton = () => {
+  // Lamamos simplemente la función cancelTask()
+  cancelTask(app);
+}
+
+const onKeyDownInput = (evt) => {
+  console.log(evt.key);
   // Pregunto si la tecla es el `Enter`
   if (evt.key === "Enter") {
     addTask(app);
-  }
+  } else
+    // Pregunto si la tecla es el `Escape`
+    if (evt.key === "Escape") {
+      cancelTask(app);
+    }
 }
