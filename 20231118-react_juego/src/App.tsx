fc6389cc60a9cd6f9761a826cf849a842eae805a
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2023-11-20 09:44:29 -0500

    feat: React NodeJS Click al tablero y cambiando el Turno
    - Dentro de "Square.tsx" llamamos la funcion "handleClick" al hacer click y este llama "updateBoard"
    - En "App.tsx" ponemos el elemento en el arreglo y cambiamos el turno.
    
    #20231118b

--- 20231118-react_juego/src/App.tsx
+++ 20231118-react_juego/src/App.tsx
@@ -8,13 +8,26 @@ const TURNS = {
 };
 
 // Creamos el Tablero en forma de arreglo
-const initBoard =Array(9).fill(null); // ['x','o','x','o','x','x','o', 'o','x'];
+const initBoard = Array(9).fill(null); // ['x','o','x','o','x','x','o', 'o','x'];
 
 function App() {
   // Ponemos el tablero en forma de estado
   const [board, setBoard] = useState(initBoard);
   // Creamos los turnos y se inicializa con el valor de la X
   const [turn, setTurn] = useState(TURNS.X);
+
+  // Función para saber q se modifica en el tablero
+  const updateBoard = (index: number) => {
+    // Antes de cambiar el turno dibujamos el elemento en el tablero
+    const newBoard = [...board] //Clonamos el arreglo llamado board
+    newBoard[index] = turn; // Asignamos el valor al índice
+    setBoard(newBoard); // Grabamos el arreglo recien obtenido
+    // Alamacenamos el Proximo turno cuando se cambia de turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    // Actualiza el estado de Turno
+    setTurn(newTurn);
+  }
+
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
@@ -22,7 +35,11 @@ function App() {
         { // Simplemente mostramos el arreglo por pantalla
           board.map((_, index) => {
             return (
-              <Square key={index} index={index}>
+              <Square
+                key={index}
+                index={index}
+                updateBoard={() => updateBoard(index)}
+              >
                 {board[index]}
               </Square>
             )
@@ -30,8 +47,8 @@ function App() {
         }
       </section>
       <section className="turn">
-        <Square isSelected={turn===TURNS.X}>{TURNS.X}</Square>
-        <Square isSelected={turn===TURNS.O}>{TURNS.O}</Square>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   );
