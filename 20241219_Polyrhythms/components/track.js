32c5a389dfe0f3d4309a7f609b96b80f23a01e57
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2024-12-20 17:04:58 -0500

    Feat: Polyrhythms. Dibujo una bola usando el `canvas` en **`ball.js`**
    - Se crea **`ball.js`** con la clase `Ball`.
    - Se importa la clase `Ball` y se usa en **`script.js`**.
    - Movemos los archivos  **`ball.js`** y **`track.js`** a la carpeta **"components"**.
    
    #03ballFile

--- 20241219_Polyrhythms/components/track.js
+++ 20241219_Polyrhythms/components/track.js
@@ -4,6 +4,14 @@ class Track{
     this.radius = radius;
   }
 
+  
+  getPosition(offset) {
+    return {
+      x: this.center.x + Math.cos(offset) * this.radius,
+      y: this.center.y + Math.sin(offset) * this.radius,
+    };
+  }
+
   draw(ctx){
     ctx.beginPath();
     ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
--- 20241219_Polyrhythms/script.js
+++ 20241219_Polyrhythms/script.js
@@ -1,4 +1,5 @@
-import Track from './track.js'; // Importo la clase `Track`
+import Track from './components/track.js'; // Importo la clase `Track`
+import Ball from './components/ball.js'; // Importo la clase `Ball`
 
   const size = 700; // tamaño del canvas  
   const myCanvas = document.getElementById('canvas'); // Obtengo el canvas
@@ -16,6 +17,11 @@ const setInit = () => {
 
   const trackCenter = { x: size / 2, y: size / 2 }; // Centro del canvas
   const trackRadius = size / 3; // Radio del círculo
+  const ballRadius = 10; // Radio de la bola
+  const ballSpeed = 0.1; // Velocidad de la bola
+
   const track = new Track(trackCenter, trackRadius); // Defino el objeto
+  const ball = new Ball(track, ballRadius, ballSpeed); // Defino el objeto
   track.draw(ctx); // Dibujo el círculo
+  ball.draw(ctx); // Dibujo la bola
 };
