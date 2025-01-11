10a03f0ec39f9a90d5870c0d41c20e79bd482b9a
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2025-01-11 18:39:41 -0500

    Feat: Christmas Calendar. Día undécimo con **`11-snowFlake.js`** - Dibujar el copo de nieve en  **`11-snowFlake.js`**. - Llamo la función `drawSnowFlake()` desde **`script.js`**.
    #14item_11SnowFlake

---
+++ 20241230-JavaScript_Christmas_Calendar/items/11-snowFlake.js
@@ -0,0 +1,24 @@
+import draw from '../utils/draw.js'; // Importo la función draw
+import color from '../utils/color.js'; // Importo la función color
+
+function drawSnowFlake(ctx, x, y, size, hue) {
+
+  ctx.lineWidth = size * 0.05; // Establezco el ancho de la línea
+  ctx.strokeStyle = color.lightest(hue); // Establezco el color de la línea
+
+  ctx.save(); // Guardo el contexto
+  ctx.translate(x, y); // Traslado el origen al centro
+  for (let i = 0; i < 6; i++) {
+    drawBranch(ctx, 0, 0, size); // Dibujo una rama
+    ctx.rotate(Math.PI / 3); // Roto el canvas 60 grados
+  }
+  ctx.restore(); // Restauro el contexto
+
+  function drawBranch(ctx, x, y, size) {
+    draw.line(ctx, x, y, x + size * 0.5, y); // Dibujo una línea horizontal
+    draw.line(ctx, x + size * 0.3, y, x + size * 0.4, y + size * 0.15); // Dibujo una línea diagonal
+    draw.line(ctx, x + size * 0.3, y, x + size * 0.4, y - size * 0.15); // Dibujo otra línea diagonal
+  }
+}
+
+export default drawSnowFlake;
