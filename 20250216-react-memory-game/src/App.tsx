4b15d9d4bfc061ba1fd93ead7dc3f6623c0064cd
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2025-02-19 14:38:14 -0500

    Feat: Memory Game React. Building the Foundation. 0:30:45 Issue with emojisData
    - Paso 1: Obtener emojis aleatorios de la API.
    - Paso 2: Duplicar emojis únicos.
    - Paso 3: Mezclar los datos de emojis.
    
    #BuildingTheFoundation/03045_Issue_with_emojisData

--- 20250216-react-memory-game/src/App.tsx
+++ 20250216-react-memory-game/src/App.tsx
@@ -4,8 +4,8 @@ import MemoryCard from './components/MemoryCard';
 
 export default function App() {
   const [isGameOn, setIsGameOn] = useState(false);
-  const [emojisData, setEmojisData] = useState([]);
-  
+  const [emojisData, setEmojisData] = useState([] as any[]);
+
   async function startGame(e: React.FormEvent) {
     e.preventDefault();
     try {
@@ -16,13 +16,49 @@ export default function App() {
         throw new Error('Could not fetch data from API');
       }
       const data = await response.json();
-      const dataSample = data.slice(0, 5);
+      // const dataSample = data.slice(0, 5);
+      const dataSlice = getDataSlice(data);
+      const emojisArray = getEmojisArray(dataSlice);
 
-      setEmojisData(dataSample);
+      setEmojisData(emojisArray);
       setIsGameOn(true);
     } catch (error) {
       console.error(error);
     }
+  }
+
+  function getDataSlice(data: any[]) {
+    const randomIndices = getRandomIndices(data);
+    const dataSlice = randomIndices.map((index) => data[index]);
+    return dataSlice;
+  }
+
+  function getRandomIndices(data: any[]) {
+    const ramdonInidicesArray = [] as number[];
+
+    for (let i = 0; i < 5; i++) {
+      const randomNum = Math.floor(Math.random() * data.length);
+      if (!ramdonInidicesArray.includes(randomNum)) {
+        ramdonInidicesArray.push(randomNum);
+      } else {
+        i--;
+      }
+    }
+
+    return ramdonInidicesArray;
+  }
+
+  function getEmojisArray(data: any[]) {
+    const pairedEmojisArray = [...data, ...data];
+
+    // Algoritmo `Fisher-Yates`
+    for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = pairedEmojisArray[i];
+      pairedEmojisArray[i] = pairedEmojisArray[j];
+      pairedEmojisArray[j] = temp;
+    }
+    return pairedEmojisArray;
   }
 
   function turnCard() {
